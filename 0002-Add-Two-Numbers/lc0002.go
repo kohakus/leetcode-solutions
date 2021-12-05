9df08149cfbf72a1c3bfeb84a5d3f96d2067744c
kohakus <iszhangyt@163.com>
2021-12-05 18:27:14 +0800

    Update lc0002.go

--- 0002-Add-Two-Numbers/lc0002.go
+++ 0002-Add-Two-Numbers/lc0002.go
@@ -8,24 +8,24 @@
  * }
  */
 
- func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	dumb := ListNode{}
-	acc := 0
-	p, p1, p2 := &dumb, l1, l2
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+    dumb := ListNode{}
+    acc := 0
+    p, p1, p2 := &dumb, l1, l2
 
-	// merge two lists
-	for p1 != nil || p2 != nil || acc > 0 {
-		if p1 != nil {
-			acc += p1.Val
-			p1 = p1.Next
-		}
-		if p2 != nil {
-			acc += p2.Val
-			p2 = p2.Next
-		}
-		p.Next = &ListNode{Val: acc % 10}
-		p = p.Next
-		acc /= 10
-	}
-	return dumb.Next
+    // merge two lists
+    for p1 != nil || p2 != nil || acc > 0 {
+        if p1 != nil {
+            acc += p1.Val
+            p1 = p1.Next
+        }
+        if p2 != nil {
+            acc += p2.Val
+            p2 = p2.Next
+        }
+        p.Next = &ListNode{Val: acc % 10}
+        p = p.Next
+        acc /= 10
+    }
+    return dumb.Next
 }
